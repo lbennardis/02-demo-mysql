c55824bcae98e46c69a33c17451b5b82883c099c
Luigi Bennardis <l.bennardis@email.it>
2016-03-03 20:11:25 +0100

    sviluppo

--- src/main/java/org/pongmatcher/repositories/PrenotazioniRepository.java
+++
@@ -1,9 +0,0 @@
-package org.pongmatcher.repositories;
-
- 
-import org.pongmatcher.domain.Prenotazioni;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PrenotazioniRepository extends JpaRepository<Prenotazioni, String> {
-
-}
--- src/main/resources/db/migration/V9__prenotazioni.sql
+++ src/main/resources/db/migration/V9__prenotazioni.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `prenotazioni` (
-  id int not null primary key,
+  id VARCHAR(255) not null primary key,
   `codice` VARCHAR(45) NOT NULL,
   `stazione` VARCHAR(255) NULL,
   `citta` VARCHAR(45) NULL,
